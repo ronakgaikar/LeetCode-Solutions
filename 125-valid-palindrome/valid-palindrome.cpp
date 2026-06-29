0893fc7ec8d3eccff40a02a14ca79968475b8558
Ronak Gaikar <94034242+ronakgaikar@users.noreply.github.com>
2026-06-29 21:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (15.97%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+    string clean_s = "";
+
+    for (int j = 0; j < s.size(); j++) {
+        if (isalnum(s[j])) {
+            clean_s += s[j];
+        }
+    }
+
+    for(int i = 0 ; i<clean_s.size()/2; i++){
+
+        clean_s[i] = tolower(clean_s[i]);
+        clean_s[clean_s.size()-i-1] = tolower(clean_s[clean_s.size()-i-1]);
+
+        if(clean_s[i] != clean_s[clean_s.size()-i-1]){
+             return false;
+        } 
+    }
+        return true; 
+
+    }
+};
