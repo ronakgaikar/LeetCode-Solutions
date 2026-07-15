789ad8c57a9eef71fd2753ad9880f962c2c4980d
Ronak Gaikar <94034242+ronakgaikar@users.noreply.github.com>
2026-07-15 23:52:08 +0530

    Time: 8 ms (13.06%) | Memory: 8.7 MB (14.21%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+         if (x == 0 || x == 1)
+            return x;
+
+        for(int i=1; i<=x; i++){
+            if((long long)i*i > x){
+                return i-1;
+            }
+        }
+
+        return x;
+
+    }
+};
