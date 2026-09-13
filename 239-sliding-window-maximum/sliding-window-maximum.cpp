68c4a8da6215e687aa23f1b058e129933aa9488a
Ronak Gaikar <94034242+ronakgaikar@users.noreply.github.com>
2026-09-13 11:37:30 +0530

    Time: 12 ms (95.11%) | Memory: 139.1 MB (74.20%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,49 @@
+//BruteForce 
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> ans;
+//         for(int i = 0; i<=n-k; i++){
+//             int maxi = INT_MIN;
+
+//             for(int j=i; j<i+k; j++){
+//                 maxi = max(maxi, nums[j]);
+//             }
+
+//             ans.push_back(maxi);
+//         }
+//         return ans;
+//     }
+// };
+
+//optimal solution
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        int n = nums.size();
+
+        for(int i = 0; i<n; i++){
+        while(!dq.empty() && dq.front()<=i-k){
+            dq.pop_front();
+        }
+
+        while(!dq.empty() && nums[dq.back()]<=nums[i]){
+            dq.pop_back();  
+        }
+
+        dq.push_back(i);
+
+        if(i>=k -1){
+            ans.push_back(nums[dq.front()]);
+        }
+
+        }
+        return ans;
+    }
+};
+
+
