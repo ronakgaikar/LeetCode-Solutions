7a67cbb15aeb7ac8c50ed2edb3a9805e4b02317f
Ronak Gaikar <94034242+ronakgaikar@users.noreply.github.com>
2026-09-18 22:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (74.67%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,18 +11,40 @@
  */
 class Solution {
 public:
+    
+    //RECURSION METHOD
+    // void preorder(TreeNode* root, vector<int> &arr){
+    //     if(root == nullptr){
+    //         return;
+    //     }
+    //     arr.push_back(root->val);
+    //     preorder(root->left, arr);
+    //     preorder(root->right, arr);
+    // }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> ans;
+    //     preorder(root,ans);
+    //     return ans;
+    // }
 
-    void preorder(TreeNode* root, vector<int> &arr){
-        if(root == nullptr){
-            return;
-        }
-        arr.push_back(root->val);
-        preorder(root->left, arr);
-        preorder(root->right, arr);
-    }
+    //ITERATIVE METHOD
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        preorder(root,ans);
-        return ans;
+        vector<int> preorder;
+        if(root == nullptr) return preorder;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+
+            preorder.push_back(root->val);
+            if(root->right != nullptr) st.push(root->right);
+            if(root->left != nullptr) st.push(root->left);
+        }
+
+        return preorder;
+ 
     }
 };
