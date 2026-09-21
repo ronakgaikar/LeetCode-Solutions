74f92ad8833512c95fcec033c88a1e37e1b150a0
Ronak Gaikar <94034242+ronakgaikar@users.noreply.github.com>
2026-09-21 18:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (93.10%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -33,26 +33,54 @@ public:
 
 
     //ITERATIVE METHOD - 2 STACKS
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> postorder;
+    //     if(root == NULL) return postorder;
+    //     stack<TreeNode*> st1,st2;
+    //     st1.push(root);
+
+    //     while(!st1.empty()){
+    //         root = st1.top();
+    //         st1.pop();
+    //         st2.push(root);
+
+    //         if(root->left != NULL) st1.push(root->left);
+    //         if(root->right != NULL) st1.push(root->right);
+    //     }
+
+    //     while(!st2.empty()){
+    //         postorder.push_back(st2.top()->val);
+    //         st2.pop();
+    //     }
+        
+
+    //     return postorder;
+    // }
+
+    //ITERATIVE METHOD - 1 STACK
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> postorder;
-        if(root == NULL) return postorder;
-        stack<TreeNode*> st1,st2;
-        st1.push(root);
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        TreeNode* lastVisited = nullptr;
 
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-
-            if(root->left != NULL) st1.push(root->left);
-            if(root->right != NULL) st1.push(root->right);
+        while(curr!=nullptr || !st.empty()){
+            if(curr!=nullptr){
+                st.push(curr);
+                curr = curr->left;
+            }
+            else{
+                TreeNode* topNode = st.top();
+                if(topNode->right != nullptr && lastVisited != topNode->right){
+                    curr = topNode->right;
+                }
+                else{
+                    postorder.push_back(topNode->val);
+                    lastVisited = topNode;
+                    st.pop();
+                }
+            }
         }
-
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
-        }
-        
 
         return postorder;
     }
